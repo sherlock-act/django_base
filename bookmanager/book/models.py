db13b7d07bfeceb3a43e376ac37b36eb288bb558
shanlei <15308255956@163.com>
2021-08-28 15:22:21 +0800

    admin站点配置

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class BookInfo(models.Model):
+    """书本类"""
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        """
+        重写str方法修改类返回信息
+        """
+        return self.name
+
+
+class PeopleInfo(models.Model):
+    """人物类"""
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """
+        重写str方法修改类返回信息
+        """
+        return self.name
--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'bookmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
