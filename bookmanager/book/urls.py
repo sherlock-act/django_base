4d827bf2d7c825f25128e332a323c2088fe8f795
shanlei <15308255956@163.com>
2021-09-01 00:28:39 +0800

    获取请求属性与自定义转换器

--- bookmanager/book/urls.py
+++ bookmanager/book/urls.py
@@ -1,7 +1,42 @@
 from django.urls import path
 from book import views
+from django.urls.converters import register_converter
+
+
+# 自定义转换器
+# 1.定义转换器
+class MobileConverter:
+    regex = '1[3-9]\d{9}'
+
+    # 验证没有问题的数据交给视图函数
+    def to_python(self, value):
+        return value
+
+    # def to_url(self, value):
+    # 将匹配结果用于反向解析传值时使用
+    #     return str(value)
+
+
+# 2.注册转换器,使用register_converter注册
+# register_converter(converter, type_name)
+# converter 转换器类
+# type_name 转换器名称
+register_converter(MobileConverter, 'phone')
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    # 获取url的路径参数
+    # http://127.0.0.1:8000/index/456/987/
+    # 获取到city_id 456  shop_id 987
+    # 通过转换器实现对数据类型的匹配
+    # 可以使用自定义的转换器实现自己的匹配需求,比如匹配手机号码
+    # 使用自定义的转换器识别手机号码
+    path('<int:city_id>/<phone:shop_id>/', views.shop),
+
+    # 获取post请求的参数
+    path('regist/', views.regist),
+
+    # 获取json数据
+    path('jsondata/', views.json_data)
 ]
--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,6 +1,9 @@
+import json
+
 from django.shortcuts import render
 from book.models import BookInfo, PeopleInfo
 from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
@@ -9,8 +12,51 @@ def index(request):
     @param request:
     @return:
     """
-    context={
+    context = {
         "name": '点击有惊喜'
     }
     return render(request, 'book/index.html', context=context)
 
+
+def shop(request, city_id, shop_id):
+    """
+    店铺
+    @param request: 请求
+    @param city_id:  城市ID
+    @param shop_id:  店铺ID
+    @return:
+    """
+    # 使用GET属性获取查询字符串参数
+    data = request.GET
+    print(data)
+
+    return HttpResponse("{}城市{}号店铺".format(city_id, shop_id))
+
+
+def regist(request):
+    """
+    获取post的form表单数据
+    @param request:
+    @return:
+    """
+    # 获取post请求中的form表单的数据,需要使用request请求的POST属性获取
+    data = request.POST
+    print(data)
+    return HttpResponse("post ok")
+
+
+def json_data(request):
+    """
+    获取json数据
+    @param request:
+    @return:
+    """
+    # 使用request对象的body属性获取发送过来的json数据
+    data = request.body
+    # print(data)
+    data_str = data.decode()
+    # print(data_str)
+    data_dict = json.loads(data_str)
+    print(data_dict)
+
+    return HttpResponse("json ok")
